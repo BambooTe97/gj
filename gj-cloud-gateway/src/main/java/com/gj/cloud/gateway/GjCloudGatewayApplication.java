bd6835b07d29147e678e1c974418b7c77379ed23
houbingyun <1421500371@qq.com>
2023-12-23 16:36:31 +0800

    新增gateway和security

---
+++ gj-cloud-gateway/src/main/java/com/gj/cloud/gateway/GjCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gj.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ gj-cloud-security/src/main/java/com/gj/cloud/security/GjCloudSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.gj.cloud.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjCloudSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjCloudSecurityApplication.class, args);
+	}
+
+}
